34e37f3d160fad19b3b70572c76086133c2c5e34
RobertZK <technoguyrob@gmail.com>
2014-10-12 21:19:45 -0500

    helper for undressing objects

--- R/utils.R
+++ R/utils.R
@@ -13,6 +13,11 @@ as.patch <- function(x) {
   x
 }
 
+bare <- function(x) {
+  attributes(x) <- NULL
+  unclass(unname(x))
+}
+
 benchmarks <- function(path) {
   if (missing(path)) {
     # TODO: (RK) Run all benchmarks in installed package.
