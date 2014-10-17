f464ddf071745a3e6078160d3c164dc2f5abdbff
RobertZK <technoguyrob@gmail.com>
2014-10-16 23:05:29 -0500

    make sure identity patch leaves most objects alone

--- inst/tests/test-patches.R
+++ inst/tests/test-patches.R
@@ -1,6 +1,12 @@
 context('patches')
 
 test_that('the identity patch has nothing in its environment', {
-  expect_identical(environment(identity_patch(NULL)), emptyenv())
+  expect_identical(environment(identity_patch()), emptyenv())
 })
+
+test_that('the identity patch patches a simple example correctly', {
+  for (obj in list(iris, 1:10, "X", function(x) y, as.environment(list(x = 1, y = 2))))
+    expect_identical(identity_patch()(obj), obj)
+})
+
 
