43c12786cad80261433ac25ec01d23a372512d18
RobertZK <technoguyrob@gmail.com>
2014-10-16 23:06:16 -0500

    make sure the trivial patch does nothing expect use the stored object

--- inst/tests/test-patches.R
+++ inst/tests/test-patches.R
@@ -9,4 +9,9 @@ test_that('the identity patch patches a simple example correctly', {
     expect_identical(identity_patch()(obj), obj)
 })
 
+test_that('the trivial patch patches a simple example correctly', {
+  for (obj in list(iris, 1:10, "X", function(x) y, as.environment(list(x = 1, y = 2))))
+    expect_identical(trivial_patch("the real deal")(obj), "the real deal")
+})
+
 
