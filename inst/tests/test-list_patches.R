66ea2fb889e3c5331581124fa9674b95e3aca20d
RobertZK <technoguyrob@gmail.com>
2014-11-17 09:05:48 -0600

    it does not take up too much space when patching a huge list with only a tiny change

--- inst/tests/test-list_patches.R
+++ inst/tests/test-list_patches.R
@@ -18,12 +18,12 @@ test_that('it can patch a huge list with only a tiny change', {
 
   expect_diff(x, y)
 })
-
 test_that('it does not take up too much space when patching a huge list with only a tiny change', {
   x <- as.list(1:10000)
   y <- x
   y[[10000]] <- 1
 
-  expect_less_than(object.size(environment(objectdiff(x, y))), 10000)
+  expect_less_than(object.size(environment(objectdiff(x, y))), 1000)
 })
+
 
