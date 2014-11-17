bc4ba460401695653dd545f96f97b92d1c7e93f8
RobertZK <technoguyrob@gmail.com>
2014-11-17 15:56:11 -0600

    begin tracked_environment idea

--- R/tracked_environment.R
+++ R/tracked_environment.R
@@ -1,20 +1,31 @@
+#' An environment object that tracks changes made to its constituents.
+#' 
+#' A typical R environment is like a mathematical set. It is a bag of 
+#' stuff and not much else. A tracked environment is like such an environment
+#' that can also replay "changes" to its history.
+#'
+#' To create a tracked environment from any environment \code{e}, simply write
+#' \code{tracked_environment(e)}.
+#' 
+tracked_environment <- function(env) {
+
+}
+
 `$<-.tracked_environment` <- function(env, name, value) {
   tmp <- class(env)
-  class(env) <- NULL
-  assign_call <- quote(`$<-`(env, name, value))
+  assign_call <- quote(`$<-`(env$env, name, value))
   assign_call[[3]] <- as.name(substitute(name))
   eval(assign_call)
-  class(env) <- tmp
   env
 }
 
 `[[<-.tracked_environment` <- function(env, name, value) {
   tmp <- class(env)
-  class(env) <- NULL
-  `[[<-`(env, name, value)
-  class(env) <- tmp
+  `[[<-`(env$env, name, value)
   env
 }
+
+
 
 
 
