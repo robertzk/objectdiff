c062d98eb792bbbf643974c95a0d939f64b68694
RobertZK <technoguyrob@gmail.com>
2014-11-17 14:45:05 -0600

    Initial idea behind tracked environments

--- R/tracked_environment.R
+++ R/tracked_environment.R
@@ -1,1 +1,19 @@
-#`$<^- 
+`$<-.tracked_environment` <- function(env, name, value) {
+  tmp <- class(env)
+  class(env) <- NULL
+  assign_call <- quote(`$<-`(env, name, value))
+  assign_call[[3]] <- as.name(substitute(name))
+  eval(assign_call)
+  class(env) <- tmp
+  env
+}
+
+`[[<-.tracked_environment` <- function(env, name, value) {
+  tmp <- class(env)
+  class(env) <- NULL
+  `[[<-`(env, name, value)
+  class(env) <- tmp
+  env
+}
+
+
