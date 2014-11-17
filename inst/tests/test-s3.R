9f7ca75567615069eb896f0321e4b46efbf089b5
RobertZK <technoguyrob@gmail.com>
2014-11-17 11:23:55 -0600

    it can diff an lm object

--- inst/tests/test-s3.R
+++ inst/tests/test-s3.R
@@ -1,9 +1,17 @@
 context('s3 objects')
+
 test_that('it can diff a trivial s3 object', {
   x <- list(1)
   class(x) <- 'boo'
   y <- x
   x[[1]] <- 2
   expect_diff(x, y)
+})
+
+test_that('it can diff an lm object', {
+  lmo <- lm(Sepal.Width ~ Sepal.Length, iris)
+  iris2 <- iris; iris2[1, 1] <- 7
+  lmo2 <- lm(Sepal.Width ~ Sepal.Length, iris2)
+  expect_diff(lmo, lmo2)
 })
 
