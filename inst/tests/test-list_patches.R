35189bcd09e1fa4f88dcfd3f316d107afea6a2d5
RobertZK <technoguyrob@gmail.com>
2014-11-17 08:57:40 -0600

    it can patch a non-trivial list to the trivial list

--- inst/tests/test-list_patches.R
+++ inst/tests/test-list_patches.R
@@ -7,3 +7,7 @@ test_that('it can patch the trivial list to itself', {
 test_that('it can patch the trivial list to a non-trivial list', {
   expect_diff(list(), as.list(1:10))
 })
+
+test_that('it can patch a non-trivial list to the trivial list', {
+  expect_diff(as.list(1:10), list())
+})
