564e94d9c4571c0664c345cb8d7e006d1aeefadc
RobertZK <technoguyrob@gmail.com>
2014-11-17 13:45:09 -0600

    add failing test for environment patching

--- inst/tests/test-environment.R
+++ inst/tests/test-environment.R
@@ -5,3 +5,13 @@ test_that('it can patch an empty environment', {
   expect_diff(x, x)
 })
 
+test_that('it can patch an environment with one variable', {
+  x <- list2env(list(x = 1))
+  x$y <- 2
+  y <- list2env(list(x = 1))
+
+  expect_false(identical(env <- objectdiff(x, y)(x), list2env(lst <- list(x = 1))))
+  expect_false(identical(env, y))
+  expect_identical(as.list(env), lst)
+})
+
