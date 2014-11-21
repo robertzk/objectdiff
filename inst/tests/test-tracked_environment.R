bc62693e480b63b4e15d471e8ce88b10f5088896
RobertZK <technoguyrob@gmail.com>
2014-11-20 22:54:36 -0600

    the meta-environment test-tracked_environment.R-tracked_environment.R infix operator works

--- inst/tests/test-tracked_environment.R
+++ inst/tests/test-tracked_environment.R
@@ -25,6 +25,11 @@ test_that("the [[ operator works", {
   expect_identical(x[['x']], 1)
 })
 
+test_that("the meta-environment %$% infix operator works", {
+  x <- tracked_environment(list2env(list(x = 1)))
+  expect_identical(as.list(x%$%env), list(x = 1))
+})
+
 test_that('it can assign to the underlying environment', {
   x <- new.env()
   tx <- tracked_environment(x)
