90db199cdaea0b086e818fc782a027e4f46c2387
RobertZK <technoguyrob@gmail.com>
2015-01-11 21:51:00 -0600

    test that it can force push to a previous commit, then force push to the future

--- inst/tests/test-force_push.R
+++ inst/tests/test-force_push.R
@@ -13,3 +13,14 @@ test_that("it can force push back to the zeroth commit", {
   expect_equal(length(ls(env)), 0)
 })
 
+test_that("it can force push to a previous commit, then force push to the future", {
+  env <- tracked_environment()
+  for (i in seq_along(letters)) {
+    env[[letters[i]]] <- i
+    commit(env) <- paste('commit', i)
+  }
+  for (i in c(15, 10, 5, 15)) {
+    force_push(env, i)
+    expect_identical(sort(ls(env)), letters[seq_len(i)])
+  }
+})
