9141392d8881de6759db0ed89b9c42aefb2c0a85
RobertZK <technoguyrob@gmail.com>
2015-01-11 21:43:33 -0600

    test that it can force push back to the zeroth commit

--- inst/tests/test-force_push.R
+++ inst/tests/test-force_push.R
@@ -6,3 +6,10 @@ test_that('it can force push to the zeroth commit', {
   assert(force_push(env, 0))
 })
 
+test_that('it can force push back to the zeroth commit', {
+  env <- tracked_environment()
+  env$x <- 1; commit(env) <- 'first'
+  force_push(env, 0)
+  expect_equal(length(ls(env)), 0)
+})
+
