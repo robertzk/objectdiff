158ef752ffca9c7d6b3fd1a31a0f2f2d834b4fa9
RobertZK <technoguyrob@gmail.com>
2015-01-11 21:42:44 -0600

    test that it can force push to the zeroth commit

--- inst/tests/test-force_push.R
+++ inst/tests/test-force_push.R
@@ -1,7 +1,8 @@
+library(testthatsomemore)
 context('force_push')
 
 test_that('it can force push to the zeroth commit', {
   env <- tracked_environment()
-  force_push(env, 0)
+  assert(force_push(env, 0))
 })
 
