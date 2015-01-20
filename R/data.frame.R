d7cbbe2364a15d9a0fe3a14866d8958a277baa9d
RobertZK <technoguyrob@gmail.com>
2015-01-20 14:40:54 -0800

    initial heuristics for data frame diffing

--- R/data.frame.R
+++ R/data.frame.R
@@ -4,8 +4,31 @@
 #' @include objectdiff.R
 setMethod('objectdiff', signature = c('data.frame', 'data.frame'),
   definition = function(old_object, new_object) {
-    if (NROW(old_object) != NROW(new_object)) { 
+    if (NROW(old_object) < NROW(new_object)) { 
+      trivial_patch(new_object)
+    } else if (NROW(old_object) > NROW(new_object)) {
+      # TODO: (RK) Use C++ for this, since it is probably slow. 
+      old_rownames <- row.names(old_object)
+      new_rownames <- row.names(new_object)
+      if (length(setdiff(new_rownames, old_rownames)) == 0) {
+        # The new row names are a subset of the old row names.
+
+        # If columns *were* modified, just do a full trivial patch or the results
+        # will certainly be wrong.
+        if (length(old_object) != length(new_object)) {
+          trivial_patch(new_object)
+        } else {
+          # To save on time, we will assume the rows themselves were
+          # not modified, or a full check could take a long time.
+          warning("Row dropping detected. Diff may not be accurate.", call. = FALSE)
+
+          trivial_patch(new_object)
+        }
+      } else {
+        trivial_patch(new_object)
+      }
+    } else {
+      getMethod("objectdiff", c("list", "list"))(old_object, new_object)
     }
-    getMethod("objectdiff", c("list", "list"))(old_object, new_object)
   })
 
