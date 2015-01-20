42e01863634e57bb51261867fd2a2276e2b5a073
RobertZK <technoguyrob@gmail.com>
2015-01-20 14:25:02 -0800

    add failing row dropping data.frame test

--- inst/tests/test-data.frame.R
+++ inst/tests/test-data.frame.R
@@ -7,3 +7,11 @@ test_that('it can patch a data.frame with a small patch', {
   # Should be even smaller
   # See: https://github.com/robertzk/objectdiff/issues/11
 })
+
+test_that('it can record dropping of rows', {
+  iris2 <- iris
+  iris2 <- iris2[-1, ]
+  expect_diff(iris, iris2)
+})
+
+
