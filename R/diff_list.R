87c2df209ba937bb2d01d56861a242eaa6d64ee8
RobertZK <technoguyrob@gmail.com>
2015-01-20 14:56:13 -0800

    placeholders for smart list diffing

---
+++ R/diff_list.R
@@ -0,0 +1,14 @@
+# TODO: (RK) Implement smart list diffing.
+
+additions.list <- function(old_object, new_object) {
+  .NotYetImplemented()
+}
+
+modifications.list <- function(old_object, new_object) {
+  .NotYetImplemented()
+}
+
+additions.list <- function(old_object, new_object) {
+  .NotYetImplemented()
+}
+
